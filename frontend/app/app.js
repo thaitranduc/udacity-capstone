185a3ab199e0a1e76314421e16075f591fe36dfb
thaitranduc <dthai9323@gmail.com>
2023-10-26 20:38:05 +0700

    update code

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send(`Capstone project ThaiTD3`)
+    res.send(#`Capstone project ThaiTD3`)
 })
 
 module.exports = app;
